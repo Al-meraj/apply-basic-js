4e8b0e7477abc9302192644d194949cb2bdcf673
Al-meraj <almeraj806@gmail.com>
2021-10-12 09:28:37 +0600

    added inchToMile

---
+++ bonus.js
@@ -0,0 +1,2 @@
+var nums = [45,85,63,95,45,47,20,77,13,49];
+console.log(nums.length);
---
+++ inchToMile.js
@@ -0,0 +1,7 @@
+function inchToMile(inch){
+    var mile = inch*63360;
+    return mile;
+}
+var apple = inchToMile(5);
+console.log(apple);
+
